5a49b015a9e814ac6307f8b9ff6eac67dd2b27e7
Kris0011 <krishp759@gmail.com>
2024-03-03 22:31:29 +0530

    allowed host

--- AroundYou/settings.py
+++ AroundYou/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-+_q(=8gmb@#@$v-2k^ps#2+kaa65*adgig#5g41=#9zcnk4%5j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
 
 # Application definition
