c9d0d810544ece7604991daa44af86b1c29119a6
Kris0011 <krishp759@gmail.com>
2024-03-06 23:35:37 +0530

    changes for vercel

--- AroundYou/urls.py
+++ AroundYou/urls.py
@@ -14,13 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path , include
+from AroundYou import settings
 from authapp import urls as auth_urls
 from service import urls as service_url
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(auth_urls)),
     path('service/',include(service_url)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 echo "Building the project..."
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "Build complete."
