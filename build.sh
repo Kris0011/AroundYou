85969d644a56c29990c0fccc37eac9b2101638a8
Kris0011 <krishp759@gmail.com>
2024-03-08 14:32:23 +0530

    check for build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 echo "Building the project..."
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 echo "Build complete."
